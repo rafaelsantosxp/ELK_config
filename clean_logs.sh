f986f63c43b7a0f39d9133c62cafed64c4eb75dc
Rafael Santos <49077225+rafaelsantosxp@users.noreply.github.com>
2020-02-14 12:17:12 -0300

    Update clean_logs.sh

--- clean_logs.sh
+++ clean_logs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm --network=host -e "ELASTICSEARCH_HOST=elasticsearch-tst.e-unicred.com.br" -e "INDEX_TO_DELETE=filebeat" -e "OLDER_THAN_IN_DAYS=7" jenciso/elasticsearch-curator
+docker run --rm --network=host -e "ELASTICSEARCH_HOST=host.test.com.uk" -e "INDEX_TO_DELETE=filebeat" -e "OLDER_THAN_IN_DAYS=7" jenciso/elasticsearch-curator
